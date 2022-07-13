4c7d4972c292b230e748404e1ebf0ab5299dbf4d
Valen <valenpschool@gmail.com>
2022-07-13 14:19:45 -0300

    feat: aula sobre comentários

---
+++ comentarios.js
@@ -0,0 +1,11 @@
+// isso é um comentário de linha
+
+// ele NÃO é interpretado, se trata de uma anotação
+
+/* esse também é um comentário, mas de bloco
+ele também não é interpretado
+é um comentário de código, para explicar algo, por exemplo
+*/ 
+
+
+// :)
