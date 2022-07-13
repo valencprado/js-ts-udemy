9b85e078e3a56ad20d48e77c9ed19d9e05da35c9
Valen <valenpschool@gmail.com>
2022-07-13 18:17:56 -0300

    feat: aula "7"

---
+++ js-basico/aula7/script.js
@@ -0,0 +1,20 @@
+// variáveis constantes (const)
+const nome = 'Valen';
+console.log('O', nome, 'não muda hein');
+
+const primeiroNumero = 2;
+const segundoNumero = 4;
+
+const conta = primeiroNumero * segundoNumero;
+const contaDuplicada = conta * 2;
+
+// + - / *
+
+
+// 1- não pode usar palavras reservadas
+// 2- nomes SIGNIFICATIVOS
+// 3- não se começa com número
+// 4- não pode ter espaço ou hífen
+// 5- camelCase
+// 6- Case Sensitive: maiúsculo/minúsculo faz diferença
+// 7 - não se reatribui valor a const
