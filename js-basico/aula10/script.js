f5ae9a5f9a519c09a88154e7814835ebf361eda6
Valen <valenpschool@gmail.com>
2022-07-15 20:47:59 -0300

    feat: aula "10"

---
+++ js-basico/aula10/script.js
@@ -0,0 +1,30 @@
+/**
+ * Aritméticos
+ * + adição ou concatenação (união de dois valores)
+ *  - / *
+ *  ** potenciação
+ *  % resto da divisão
+ */
+
+const num1 = 5;
+const num2 = 2;
+
+// console.log(num1 + num2); // adição
+// console.log(num1 - num2); // subtração
+// console.log(num1 * num2); // multiplicação
+// console.log(num1 ** num2); // potenciação
+// console.log(num1 / num2); // divisão
+// console.log(num1 % num2); // resto da divisão
+
+/** prioridade para a multiplicação/divisão (precedência)
+ * para alterar a precedência -> parênteses
+*/
+
+let contador = 1;
+contador++; // apenas com const
+++contador;
+// ou
+contador--;
+--contador;
+
+contador *=3; // contador igual a contador vezes 3
