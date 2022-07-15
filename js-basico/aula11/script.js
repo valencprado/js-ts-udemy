4dfbda5bd2a2e98dc33967181b6d88c1a55fa7a5
Valen <valenpschool@gmail.com>
2022-07-15 20:48:09 -0300

    feat: aula "11"

---
+++ js-basico/aula11/script.js
@@ -0,0 +1,10 @@
+
+// exercício passado no fim da aula
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+let resultado = num1 + num2;
+
+alert(`O resultado foi: ${resultado}`)
+
