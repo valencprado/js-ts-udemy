de6315b482c6df52cdf2de39d1bc6578d0c08de7
Valen <valenpschool@gmail.com>
2023-01-12 16:50:37 -0300

    feat: aula "24"

---
+++ logica-programacao/aula24/script.js
@@ -0,0 +1,34 @@
+// if/else if/else
+// exemplo: sistema que dá bom dia até certo horário
+const hora = 1;
+
+// if(hora <12){
+// console.log('Bom dia!');
+// }
+// ou seja, se for verdadeira, será dito o bom dia
+// caso contrário, o bloco não será executado
+
+// if(hora >= 0 && hora <=11){
+//  console.log('Bom dia!');
+//  } else if(hora >= 12 && hora <= 17){
+//  console.log('Boa tarde!');
+//  } else if(hora >=18 && hora <= 23){
+//  console.log('Boa noite!');
+//  } else{
+// // nesse caso, foi digitada uma hora errada, como 50
+// console.log('Oi, independente de que horas sejam!');
+//  }
+
+ // if pode ser usado sozinho
+ // antes de um else SEMPRE existe um if 
+ // else if pode ser criado várias vezes na checagem
+// é possível usar só if e else ou somente if
+
+
+//  const tenhoGrana = true;
+ 
+//  if(tenhoGrana){
+//     console.log('Bora sair!')
+//  } else{
+//     console.log('Vou ter que ficar em casa...')
+//  }
