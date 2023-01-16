70441d54fc2a6bdfb7a9add23531e0e215ae65e7
Valen <valenpschool@gmail.com>
2023-01-16 13:14:47 -0300

    feat: aula "27"

---
+++ logica-programacao/aula27/script.js
@@ -0,0 +1,11 @@
+// operação ternária
+// const pontuacaoUsuario = 999;
+// if(pontuacaoUsuario >= 1000){
+//     console.log('VIP')
+// }else{
+//     console.log('Normal')
+// }
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'VIP': 'Normal';
+console.log(nivelUsuario);
