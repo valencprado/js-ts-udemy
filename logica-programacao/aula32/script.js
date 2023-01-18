a40dc3341c6572ad7102d4c402a325532b2e6e0e
Valen <valenpschool@gmail.com>
2023-01-17 23:15:13 -0300

    fix: vírgulas

--- logica-programacao/aula32/script.js
+++ logica-programacao/aula32/script.js
@@ -1,4 +1,4 @@
-const numeros = [[1, 2, 3] [4, 5, 6][7, 8, 9]] // útil
+const numeros = [[1, 2, 3],[4, 5, 6],[7, 8, 9]] // útil
 //const [,[,,seis]] = numeros; //complexo
 const [lista1, lista2, lista3] = numeros;
 console.log(lista1[1]);
