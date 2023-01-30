111bbf72c0cc622b627372bedcdbebce7e2fa3e2
Valen <valenpschool@gmail.com>
2023-01-29 21:56:40 -0300

    feat: exercício da aula "42" (feito)

--- logica-programacao/aula42/script.js
+++ logica-programacao/aula42/script.js
@@ -1,2 +1,9 @@
 // função chamada ePaisagem que recebe largura e altura de uma imagem
 // retornar true se a imagem estiver no modo paisagem
+
+function ePaisagem(largura, altura){
+    const paisagem = largura > altura ?? true;
+    return paisagem
+}
+
+console.log(ePaisagem(1000, 100))
