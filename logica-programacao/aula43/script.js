347b97d60d388f2fd115c8e747b080111b66af07
Valen <valenpschool@gmail.com>
2023-01-30 12:28:33 -0300

    exercício da aula "43"

--- logica-programacao/aula43/script.js
+++ logica-programacao/aula43/script.js
@@ -3,5 +3,39 @@
 // divisível por 5 = Buzz
 // divisível por 3 e 5 = FizzBuzz
 // NÃO divide por 3 e 5 = retorna o número
-// hecar se o número é realmente um número
+// checar se o número é realmente um número
 // função com números de 0 a 100
+
+const numero = Math.round(Math.random() * 100);
+ console.log(numero);
+
+function fizzBuzz(){
+    
+if((numero % 3) === 0 & (numero % 5) === 0){
+  console.log('FizzBuzz');
+}else if((numero % 3) === 0){
+  console.log('Buzz');
+}else if((numero % 5) === 0){
+  console.log('FizzBuzz');
+}else if((numero% 3) !== 0 & (numero % 5) !== 0){
+  console.log(numero);
+}
+
+// switch não funcionando
+/*
+ switch(numero){
+     case (numero % 3) === 0 & (numero % 5) === 0:
+         console.log('FizzBuzz');
+         break;
+     case (numero % 3) === 0:
+         console.log('Fizz');
+         break;
+     case (numero % 5) === 0:
+         console.log('Buzz');
+         break;
+     case (numero % 3) !== 0 & (numero % 5) !== 0:
+         console.log(numero);
+ }
+*/
+}
+fizzBuzz();
