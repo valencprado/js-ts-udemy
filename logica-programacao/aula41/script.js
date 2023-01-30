da01a238adac62783af73b677d03c25bad588110
Valen <valenpschool@gmail.com>
2023-01-30 12:34:05 -0300

    feat: exercício corrigido

--- logica-programacao/aula41/script.js
+++ logica-programacao/aula41/script.js
@@ -3,4 +3,10 @@
 function maiorNumero(numero1, numero2) {
     return Math.max(numero1, numero2);
 }
-console.log(maiorNumero(5, 10))
+console.log(maiorNumero(5, 10));
+
+// correção do professor
+/**
+ * const max2 = (x, y) => x > y ? x:y;
+ * console.log(max2(10, 5));
+ */
