ad17ea777a33a20d332b5f6aee488a173d49f9c4
Valen <valenpschool@gmail.com>
2023-01-30 12:52:06 -0300

    exercício corrigido

--- logica-programacao/aula42/script.js
+++ logica-programacao/aula42/script.js
@@ -1,9 +1,5 @@
 // função chamada ePaisagem que recebe largura e altura de uma imagem
 // retornar true se a imagem estiver no modo paisagem
 
-function ePaisagem(largura, altura){
-    const paisagem = largura > altura ?? true;
-    return paisagem
-}
-
-console.log(ePaisagem(1000, 100))
+const ePaisagem = (largura, altura) => largura >= altura;
+console.log(ePaisagem(1080, 1920));
