8c87087672ea229b240af09063c7aa57a8c98d08
Valen <valenpschool@gmail.com>
2023-02-02 16:43:24 -0300

    feat: exercício (feito)

--- logica-programacao/exercicio-timer/assets/js/script.js
+++ logica-programacao/exercicio-timer/assets/js/script.js
@@ -3,19 +3,40 @@ const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
-let minuto = 0;
-let segundo = 0;
-let milissegundo = 0;
-iniciar.addEventListener('click', function(event){
-   const tempo =  setInterval(function(){
-        
-    }, 1000)
+let hora = 0;
+let min = 0;
+let seg = 0;
+
+function ticTac(){
+   seg++;
+   if(seg === 60){
+      seg = 0;
+      mm++;
+   }
+   if(min === 60){
+      min = 0;
+      hora++;
+   }
+   relogio.innerHTML = `${hora < 10 ? '0' + hora:hora}:${min < 10 ? '0' + min:min}:${seg < 10 ? '0' + seg:seg}`;
+}
+let timer;
+iniciar.addEventListener('click', event => {
+ timer = setInterval(() => {
+    ticTac();
+
+}, 1000)
 });
-pausar.addEventListener('click', function(event){
+pausar.addEventListener('click', event =>{
      setTimeout(function(){
-            clearInterval(tempo);
+            clearInterval(timer);
     }, 0)
-});
-zerar.addEventListener('click', function(event){
-    alert('zerar');
+    relogio.className = 'relogio pausa';
+   });
+   zerar.addEventListener('click', event => {
+      clearInterval(timer);
+      hora = 0;
+      min = 0;
+      seg = 0;
+      relogio.innerHTML = `00:00:00`;
+      relogio.className = 'relogio';
 });
