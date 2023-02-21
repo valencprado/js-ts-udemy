a6f5fed677273883723748f4dff5f596a8bb2202
Valen <valenpschool@gmail.com>
2023-02-21 14:54:45 -0300

    feat: aula "82"

---
+++ 4-arrays-avancado/82/script.js
@@ -0,0 +1,10 @@
+// método splice()
+const comidas = ['Hambúrguer', 'Batata', 'Arroz', 'Macarrão', 'Tomate'];
+console.log(comidas);
+
+
+// const removidos = comidas.splice(0, 1); // shift
+// const removidos = comidas.splice(-1, 1); // pop
+// comidas.splice(5, 0, 'Lasanha') // push
+// comidas.splice(0, 0, 'Lasanha') // unshift
+
