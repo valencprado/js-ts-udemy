60c148e3af2ff5280e2e7d0ad5ee130a86dbe240
Valen <valenpschool@gmail.com>
2023-02-21 10:45:50 -0300

    feat: aula "81"

---
+++ 4-arrays-avancado/81/script.js
@@ -0,0 +1,22 @@
+// o que já foi visto
+// const nomes2 = new Array('a', 'b', 'c'); // array com construtor
+//delete nomes[1];
+// const nomes = ['a', 'b', 'c']; // array literal
+// const novo = nomes.slice(1, -1)
+// console.log(novo);
+
+const nomes = [ 'Valentina', 'Prado', '123' ];
+const nome = nomes.join(' ');
+console.log(nome);
+
+
+
+//console.log(nomes.length); // tamanho
+//nomes.push('d');
+//const removido = nomes.pop(); // remove o último
+//const removido = nomes.shift(); // remove o primeiro
+// const novo = [...nomes];
+// //nomes[2] = 'd';
+// novo.pop();
+// console.log(nomes);
+// console.log(novo);
