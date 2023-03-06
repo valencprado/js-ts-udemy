e2af7ca152533602e860a2720575398bf9c26876
Valen <valenpschool@gmail.com>
2023-03-06 11:49:12 -0300

    fix: add título da aula

--- 4-arrays-avancado/86/script.js
+++ 4-arrays-avancado/86/script.js
@@ -1,4 +1,5 @@
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// reduce()
+
 const total = numeros.reduce((acumulador, valor, indice, array) => {
    if(valor % 2 === 0) {
     acumulador += valor;
