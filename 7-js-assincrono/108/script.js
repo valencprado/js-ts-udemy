e552cfb8664621eea8bcfa44b37eb9b62403f92f
Valen <valenpschool@gmail.com>
2023-03-13 16:49:29 -0300

    feat: aula "108"

---
+++ 7-js-assincrono/108/script.js
@@ -0,0 +1,44 @@
+// promises
+function geraTempo(min, max) {
+    min *= 1000;
+    max *= 1000;
+return Math.floor(Math.random() * (max-min) + min)
+}
+function espere(msg, tempo) {
+
+    return new Promise((res, rej) => {
+        if(typeof msg !== 'string') rej('UEPA');
+        setTimeout(() => {
+            res(msg);
+        }, tempo);
+    });
+   
+}
+
+espere('Conexão com BD', geraTempo(1, 3))
+    .then(resposta => {
+        console.log(resposta);
+        return espere('Buscando dados da BD', geraTempo(1, 3));
+    })
+    .then(resposta => { 
+        console.log(resposta);
+        return espere(3443, geraTempo(1, 3));
+    })
+    .then((resposta) => {
+        console.log(resposta);
+    })
+    .then(() => {
+        console.log('Exibe os dados');
+    })
+    .catch(e => {
+        console.log('ERRO', e);
+    })
+
+
+// espere('frase a', geraTempo(1, 3), function() {
+//     espere('frase b', geraTempo(1, 3), function() {
+//      espere('frase c', geraTempo(1, 3));
+
+// });
+
+// });
