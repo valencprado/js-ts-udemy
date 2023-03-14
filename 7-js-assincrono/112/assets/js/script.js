4823147298d3b175e43798f8eaa387a31ece2b31
Valen <valenpschool@gmail.com>
2023-03-14 16:12:53 -0300

    feat: aula "112"

---
+++ 7-js-assincrono/112/assets/js/script.js
@@ -0,0 +1,47 @@
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
+
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject({
+//                     code: xhr.status,
+//                     msg: xhr.statusText
+//                 });
+//             }
+//         })
+//     })
+
+// };
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+
+    try {
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+        if (response.status !== 200) throw new Error('erro 404');
+        const html = await response.text();
+        carregaResultado(html);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
