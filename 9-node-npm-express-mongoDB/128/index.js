d7cd68b402d633ded809b52ab7a0cc3c7d44af40
Valen <valenpschool@gmail.com>
2023-03-20 16:38:52 -0300

    feat: aula "128"

---
+++ 9-node-npm-express-mongoDB/128/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+// fs.readdir(path.resolve(__dirname))
+// .then(files => console.log(files))
+// .catch(e => console.log(e));
+
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir);
+    walk(files, rootDir);
+}
+
+async function walk(files, rootDir) {
+    for(let file of files) {
+        const fileFullPath = path.resolve(rootDir, file);
+    const stats = await fs.stat(fileFullPath);
+    if(/\.git/.test(fileFullPath)) continue;
+    if(/\.node_modules/.test(fileFullPath)) continue;
+    if(stats.isDirectory()) {
+        readdir(fileFullPath);
+        continue;
+    }
+    if(!/\.html/.test(fileFullPath) &&
+        !/\.css/.test(fileFullPath)) continue;
+    console.log(fileFullPath);
+}
+}
+readdir('');
