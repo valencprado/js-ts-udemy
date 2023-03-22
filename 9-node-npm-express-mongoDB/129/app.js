85996453fe45e019e1f6a3a037dc8274caba1153
Valen <valenpschool@gmail.com>
2023-03-22 16:09:07 -0300

    feat: aula "129" (concluída)

--- 9-node-npm-express-mongoDB/129/app.js
+++ 9-node-npm-express-mongoDB/129/app.js
@@ -3,21 +3,23 @@ const caminhoArquivo = path.resolve(__dirname, 'teste.txt');
 const escreve = require('./modules/escrever');
 const ler = require('./modules/ler');
 
-// const pessoas = [
-//     {nome: 'a'},
-//     {nome: 'b'},
-//     {nome: 'c'},
-//     {nome: 'd'},
-//     {nome: 'e'},
-// ];
-// const json = JSON.stringify(pessoas, '', 2);
-// escreve(caminhoArquivo, json);
+const pessoas = [
+    {nome: 'a'},
+    {nome: 'b'},
+    {nome: 'c'},
+    {nome: 'd'},
+    {nome: 'e'},
+];
+const json = JSON.stringify(pessoas, '', 2);
+escreve(caminhoArquivo, json);
 
 async function leArquivo(caminho) {
     const dados = await ler();
-    return dados;
+   renderizaDados(dados);
 }
-const dados = leArquivo(caminhoArquivo)
-.then(dados => {
-    console.log(dados);
-});
+
+function renderizaDados(dados) {
+   dados = JSON.parse(dados);
+   dados.forEach(val => console.log(val.nome));
+}
+leArquivo(caminhoArquivo);
