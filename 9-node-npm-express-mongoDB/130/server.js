7fd18509c09a8f18c0868368bf25d04f11bef017
Valen <valenpschool@gmail.com>
2023-03-28 16:58:56 -0300

    feat: aula "130" (finalizada)

--- 9-node-npm-express-mongoDB/130/server.js
+++ 9-node-npm-express-mongoDB/130/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `
+    );
 });
-
+app.post('/', (req, res) => {
+    res.send('Formulário recebido!')
+})
+app.get('/contato', (req, res) => {
+    res.send('Contato')
+})
 app.listen(3000, () => {
-    console.log("Acessar https://localhost:3000");
+    console.log("Acessar http://localhost:3000");
     console.log("Rodando na porta 3000!");
 });
