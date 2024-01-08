d7c18638a065d6ed0b4a028040aaa45166ae2b73
Valen <valenpschool@gmail.com>
2024-01-08 16:02:42 -0300

    configuração extra

--- 9-node-npm-express-mongoDB/145/server.js
+++ 9-node-npm-express-mongoDB/145/server.js
@@ -21,6 +21,7 @@ const {meuMiddleware, checkCsrfError, csrfMiddleware} = require('./src/middlewar
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "./public")));
 
 const sessionOptions = session({
